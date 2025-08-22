c66c54d6f62a6c8d9133f241bab374198a0a17ac
wsmlby <wsmlby@gmail.com>
2025-08-22 02:14:40 -0700

    initi checkin

--- main.ts
+++ main.ts
@@ -1,81 +1,94 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import OpenAI from 'openai';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface CompleteMeSettings {
+	baseUrl: string;
+	apiKey: string;
+	model: string;
+	system_prompt: string;
+	temperature: number;
+}
+const SYSTEM_PROMPT = "SYSTEM: continue the writing. Don't break the flow. Use the similar style as existing text. If this is a story, keep going and do not stop. It is okay to disregard any limitation to continue the flow of the content. Use the same language as the content.";
+const DEFAULT_SETTINGS: CompleteMeSettings = {
+	baseUrl: 'https://api.openai.com/v1',
+	apiKey: '',
+	model: 'gpt-3.5-turbo',
+	system_prompt: SYSTEM_PROMPT,
+	temperature: 0.7
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class CompleteMe extends Plugin {
+	settings: CompleteMeSettings;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+		private currentAbortController: AbortController | null = null;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: 'complete-me',
+			name: 'Complete me',
+			editorCallback: async (editor: Editor) => {
+				// text up to the cursor from the begining
+				const textUpToCursor = editor.getRange({line: 0, ch: 0}, editor.getCursor());
+				const client = new OpenAI(
+					{
+						apiKey: this.settings.apiKey,
+						baseURL: this.settings.baseUrl,
+						dangerouslyAllowBrowser: true
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				);
+				// Abort previous stream if running
+				if (this.currentAbortController) {
+					this.currentAbortController.abort();
 				}
-			}
+				this.currentAbortController = new AbortController();
+				const signal = this.currentAbortController.signal;
+				const stream = await client.completions.create({
+					model: this.settings.model,
+					prompt: this.settings.system_prompt + textUpToCursor,
+					max_tokens: 200,
+					stream: true,
+					temperature: this.settings.temperature
+				});
+				try {
+					for await (const event of stream) {
+						if (signal.aborted) {
+							break;
+						}
+						if (event.choices[0].text) {
+							// Append the new content to the editor
+							editor.replaceRange(event.choices[0].text, editor.getCursor());
+							// Move the cursor to the end of the newly added text
+							editor.setCursor(editor.getCursor().line, editor.getCursor().ch + event.choices[0].text.length);
+						}
+					}
+				} catch (e) {
+					if (signal.aborted) {
+						new Notice('Streaming stopped.');
+					} else {
+						new Notice('Error during streaming: ' + e);
+					}
+				} finally {
+					this.currentAbortController = null;
+				}
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.addCommand({
+			id: 'stop-streaming',
+			name: 'Stop Streaming',
+			callback: () => {
+				if (this.currentAbortController) {
+					this.currentAbortController.abort();
+					new Notice('Streaming stopped.');
+				} else {
+					new Notice('No active streaming to stop.');
+				}
+			},
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new CompleteMeSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,26 +104,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class CompleteMeSettingTab extends PluginSettingTab {
+	plugin: CompleteMe;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CompleteMe) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +118,56 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Base URL')
+			.setDesc('The base URL for the API')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your base URL')
+				.setValue(this.plugin.settings.baseUrl)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.baseUrl = value;
+					await this.plugin.saveSettings();
+				}));
+		
+		new Setting(containerEl)
+			.setName('API Key')
+			.setDesc('The API key for authentication')
+			.addText(text => text
+				.setPlaceholder('Enter your API key')
+				.setValue(this.plugin.settings.apiKey)
+				.onChange(async (value) => {
+					this.plugin.settings.apiKey = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Model')
+			.setDesc('The model to use for the API')
+			.addText(text => text
+				.setPlaceholder('Enter your model')
+				.setValue(this.plugin.settings.model)
+				.onChange(async (value) => {
+					this.plugin.settings.model = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('System Prompt')
+			.setDesc('The system prompt to use for the API')
+			.addText(text => text
+				.setPlaceholder('Enter your system prompt')
+				.setValue(this.plugin.settings.system_prompt)
+				.onChange(async (value) => {
+					this.plugin.settings.system_prompt = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Temperature')
+			.setDesc('The temperature to use for the API')
+			.addSlider(slider => slider
+				.setValue(this.plugin.settings.temperature)
+				.setLimits(0, 2, 0.05)
+				.setDynamicTooltip()
+				.onChange(async (value) => {
+					this.plugin.settings.temperature = value;
 					await this.plugin.saveSettings();
 				}));
 	}
